bc95a2b043258b8b1323f06976b7ccf26720a31b
csoni06 <103045183+csoni06@users.noreply.github.com>
2024-02-14 14:56:34 +0100

    css uptade

--- quiz/src/components/Option.js
+++ quiz/src/components/Option.js
@@ -1,11 +1,10 @@
 // Option.js
 
 import React, { Component } from 'react';
+import './Options.css'; // Adjust the path based on your file structure
 
 class Options extends Component {
-    // Helper function to handle option clicks
     handleOptionClick = (value) => {
-        // Programmatically trigger the onChange handler
         this.props.onOptionChange({
             target: { value: value },
         });
@@ -15,22 +14,31 @@ class Options extends Component {
         const { options, selectedOption, correctAnswer, showHint } = this.props;
 
         return (
-            <div className='options'>
+            <div className='options-container'>
                 {options.map((option, index) => (
-                    <div key={index} className="form-check" onClick={() => this.handleOptionClick(option)} style={{ cursor: 'pointer' }}>
+                    <div key={index} 
+                         className="form-check" 
+                         onClick={() => this.handleOptionClick(option)} 
+                         style={{ 
+                            cursor: 'pointer', 
+                            backgroundColor: selectedOption === option ? '#5ccfd1' : 'white', // Change background color if selected
+                            margin: '10px 0',
+                            padding: '10px',
+                            border: '1px solid #ddd',
+                            borderRadius: '4px',
+                         }}>
                         <input
                             type="radio"
                             name="option"
                             value={option}
                             checked={selectedOption === option}
-                            onChange={() => {}} // Keep this to avoid React controlled component warning
+                            onChange={() => {}} // This is to avoid React controlled component warning
                             className="form-check-input"
                             style={{pointerEvents: "none"}} // Prevent direct interaction with the radio button
                         />
-                        <label className="form-check-label" style={{marginLeft: "8px"}}>{option}</label>
+                        <label className="form-check-label" style={{marginLeft: "8px", color: selectedOption === option ? 'white' : '#333'}}>{option}</label>
                     </div>
                 ))}
-                {/* Conditionally rendering the correct answer hint based on showHint */}
                 {showHint && (
                     <div className="correct-answer-hint">
                         <strong>Hint: </strong>The correct answer is {correctAnswer}.
